4bca4aec26ed3c7a5e11053cc96883364a42832b
denys.havryliv <bivi325@gmail.com>
2019-05-11 22:05:20 +0300

    Added loading, Menu scene, audio

--- src/main.js
+++ src/main.js
@@ -4,12 +4,15 @@ import { LoadScene } from './scenes/LoadScene';
 import { MenuScene } from './scenes/MenuScene';
 
 const config = {
-    width: 1400,
-    height: 600,
+    width: 1366,
+    height: 768,
     parent: 'game',
     scene: [
         LoadScene, MenuScene
     ],
+    render: {
+        pixelArt : true
+    }
 }
 
 let game = new Phaser.Game(config);
